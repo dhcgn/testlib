1319e7f4636498a7c2eead392cb2909163ad0a37
Daniel <dhcgn@users.noreply.github.com>
2019-08-27 12:30:04 +0200

    Changed from netstandard2.0 to v4.7.2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace TestLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and bye in 5s");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
+}
