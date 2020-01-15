bf32ebd829600cf72437dfd3948e7e32419e6383
Hemmerling <daniel.hemmerling@haevg-rz.ag>
2020-01-15 11:32:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Threading;
+using Org.BouncyCastle.Crypto;
+using Jose;
+using Newtonsoft.Json;
+using Saxon.Api;
+using SixLabors;
 
 namespace TestLib
 {
@@ -7,6 +12,16 @@ namespace TestLib
     {
         static void Main(string[] args)
         {
+            Console.Out.WriteLine("START Namesspaces");
+
+            Console.Out.WriteLine(typeof(Org.BouncyCastle.Crypto.CryptoException).Namespace);
+            Console.Out.WriteLine(typeof(Jose.AesGcm).Namespace);
+            Console.Out.WriteLine(typeof(Newtonsoft.Json.JsonArrayAttribute).Namespace);
+            Console.Out.WriteLine(typeof(Saxon.Api.DocumentBuilder).Namespace);
+            Console.Out.WriteLine(typeof(SixLabors.GeometryUtilities).Namespace);
+
+            Console.Out.WriteLine("END Namesspaces\r\n");
+
             Console.WriteLine("Hello and bye in 5s");
             Thread.Sleep(TimeSpan.FromSeconds(5));
         }
